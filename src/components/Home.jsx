88ca871d6b56862b126357340d620f6f8bf18953
Ayush1M <worklife2442@gmail.com>
2023-07-10 23:26:38 +0530

    Added content for the home and projects route.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,89 @@
 import React from "react";
-import AvatarImg from "../images/my-image.jpg"
-import Typewriter from 'typewriter-effect'
+import AvatarImg from "../images/my-image.jpg";
+import Typewriter from "typewriter-effect";
+import { DiGithubBadge } from "react-icons/di";
+import { FaLinkedin } from "react-icons/fa";
+import efficiencyimg from "../images/efficiency.png"
+import goalsimg from "../images/goals.png"
+import flexibilityimg from "../images/flexibility.png"
+import html from "../images/icons/html-icon.png"
+import css from "../images/icons/css-icon.png"
+import javaScript from "../images/icons/javascript-icon.png"
+import react from "../images/icons/react-icon.png"
+import tailwind from "../images/icons/tailwindcss-icon.png"
+import api from "../images/icons/restapi-icon.png"
+import github from "../images/icons/github-icon.png"
+import figma from "../images/icons/figma-icon.png"
+import vscode from "../images/icons/vscode-icon.png"
+import firebase from "../images/icons/firebase-icon.png"
+import responsivedesign from "../images/icons/responsivedesign-icon.png"
 
-export default function Home(){
-    return(
-        <div className="text-white">
-            <div>
-            <h1>Hi, I'm Ayush Mahajan</h1>
-            <div className="flex">
-            <h2 className="mr-2">I am a </h2>
-            <Typewriter
-            options={{
-                strings : ["Frontend Developer"],
-                autoStart : true,
-                loop : true,
-            }} />
+export default function Home() {
+  return (
+    <>
+      <section className="text-white flex justify-evenly mt-24">
+        <div>
+          <div className="tracking-wider">
+            <h1 className="text-5xl">Hi, I'm Ayush Mahajan</h1>
+            <div className="flex mt-6">
+              <h2 className="mr-2 text-4xl">I am a </h2>
+              <div className="text-orange-500 text-4xl">
+                <Typewriter
+                  options={{
+                    strings: ["Frontend Developer!!"],
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </div>
             </div>
-            
+            <div className="flex items-center mt-16">
+              <a className="bg-orange-500 text-black px-16 py-4 rounded-lg flex items-center text-2xl mr-6 relative z-10 before:absolute before:inset-0 before:bg-black before:-z-10 before:border-2 before:border-orange-500 before:border-solid before:rounded-lg before:origin-left before:scale-x-0 before:transition-transform before:hover:scale-x-100 hover:text-orange-500" href="https://github.com/Ayush1M" target="_blank" rel="noopener noreferrer"><DiGithubBadge className="text-4xl mr-2" />Github</a>
+
+              <a className="bg-orange-500 text-black px-16 py-4 rounded-lg flex items-center text-2xl mr-6 relative z-10 before:absolute before:inset-0 before:bg-black before:-z-10 before:border-2 before:border-orange-500 before:border-solid before:rounded-lg before:origin-left before:scale-x-0 before:transition-transform before:hover:scale-x-100 hover:text-orange-500" href="https://www.linkedin.com/in/ayush-mahajan10" target="_blank" rel="noopener noreferrer"><FaLinkedin className="text-4xl mr-2" />Linkedin</a>
             </div>
-            <img className="w-[20%] rounded" src={AvatarImg} /> 
+
+          </div>
         </div>
-    )
+        <img className="w-96" src={AvatarImg} />
+      </section>
+      
+      <div className="mx-2">
+        <h2 className="text-white mt-64 text-center text-5xl">Why Should you Hire Me?</h2>
+        <section className="text-white grid grid-cols-3 text-3xl mt-24">
+        <div className="text-center">
+            <h3 className="mb-8 text-orange-500">Efficiency</h3>
+            <img src={efficiencyimg} alt="Efficiency" class="m-auto w-44 mb-8" />
+            <p class="w-[60%] m-auto max-lg:w-full">I believe having a proper planning and a end goal helps in achieving task more efficiently and takes less amout of time.</p>
+          </div>
+          <div className="text-center">
+            <h3 className="mb-8 text-orange-500">Goal Orientation</h3>
+            <img src={goalsimg} alt="goal Orientation" class="m-auto w-44 mb-8" />
+            <p class="w-[60%] m-auto max-lg:w-full">I always try to break bigger goals into smaller tasks which makes it easy to achieve them.</p>
+          </div>
+          <div className="text-center">
+            <h3 className="mb-8 text-orange-500">Flexibilty</h3>
+            <img src={flexibilityimg} alt="flexibility" class="m-auto w-44 mb-8" />
+            <p class="w-[60%] m-auto max-lg:w-full">I am flexible in working in any environment wheather its in a team or as a individual.</p>
+          </div>
+        </section>
+      </div>
+      <section className="mb-24 mt-48">
+        <h2 className="text-white mb-16 text-6xl text-center tracking-wide">Technolgies I Use</h2>
+        <div className="text-white flex justify-center flex-wrap gap-8">
+        <p className="text-center text-xl mr-8"><img className="hover:animate-spin mb-6" src={html} alt="" />Html</p>
+        <p className="text-center text-xl mr-8"><img className="hover:animate-spin mb-6" src={css} alt="" />CSS</p>
+        <p className="text-center text-xl mr-8"><img className="hover:animate-spin mb-6" src={javaScript} alt="" />JavaScript</p>
+        <p className="text-center text-xl mr-8"><img className="hover:animate-spin mb-6" src={react} alt="" />React</p>
+        <p className="text-center text-xl mr-8"><img className="hover:animate-spin mb-6" src={tailwind} alt="" />Tailwind CSS</p>
+        <p className="text-center text-xl mr-8"><img className="hover:animate-spin mb-6" src={api} alt="" />RestAPI</p>
+        <p className="text-center text-xl mr-8"><img className="hover:animate-spin mb-6" src={github} alt="" />Github</p>
+        <p className="text-center text-xl mr-8"><img className="hover:animate-spin mb-6" src={figma} alt="" />Figma</p>
+        <p className="text-center text-xl mr-8"><img className="hover:animate-spin mb-6" src={vscode} alt="" />VSCode</p>
+        <p className="text-center text-xl mr-8"><img className="hover:animate-spin mb-6" src={firebase} alt="" />Firebase</p>
+        <p className="text-xl"><img className="m-auto w-20 hover:animate-spin mb-6" src={responsivedesign} alt="" />Responsive Design</p>
+        </div>
+      </section>
+    </>
+  );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,85 @@
 import React from "react";
+import project1 from "../projects/project-image-1.png"
+import project2 from "../projects/project-image-2.png"
+import project3 from "../projects/project-image-3.png"
+import project4 from "../projects/project-image-4.png"
+import project5 from "../projects/project-image-5.png"
+import react from "../images/icons/react-icon.png"
+import css from "../images/icons/css-icon.png"
+import html from "../images/icons/html-icon.png"
+import javaScript from "../images/icons/javascript-icon.png"
+import api from "../images/icons/restapi-icon.png"
+import { BsArrowRight } from "react-icons/bs"
 
 export default function Projects(){
     return(
         <>
-        <h1>Project</h1>
+        <h1 className="text-orange-500 text-center text-6xl mt-16">My Work</h1>
+        <section className="grid grid-cols-3 gap-8 w-11/12 mt-20 m-auto">
+            <div className="border-2 border-orange-500 rounded-lg bg-black grid grid-cols-1">
+                <img className="rounded-t-lg" src={project1} alt="" />
+                <p className="text-white text-2xl mt-6 mx-4">A fun quiz app made with React and React Router.</p>
+                <p className="flex items-center text-xl text-orange-500 ml-4 mt-8">Made with : <img className="w-12 ml-4" src={react} alt="" /><img className="w-12" src={css} alt="" /><img className="w-10" src={api} alt="" /></p>
+                <div className="flex items-center ml-4 mt-10 mb-6">
+                <a className="bg-orange-500 text-black px-16 py-4 rounded-lg flex items-center justify-center w-1/5 text-2xl mr-6 relative z-10 before:absolute before:inset-0 before:bg-black before:-z-10 before:border-2 before:border-orange-500 before:border-solid before:rounded-lg before:origin-left before:scale-x-0 before:transition-transform before:hover:scale-x-100 hover:text-orange-500" href="" target="_blank" rel="noopener noreferrer">Live</a>
+
+                <a className="bg-orange-500 text-black px-16 py-4 rounded-lg w-1/5 flex items-center justify-center text-2xl relative z-10 before:absolute before:inset-0 before:bg-black before:-z-10 before:border-2 before:border-orange-500 before:border-solid before:rounded-lg before:origin-left before:scale-x-0 before:transition-transform before:hover:scale-x-100 hover:text-orange-500" href="" target="_blank" rel="noopener noreferrer">Github</a>
+                </div>
+            </div>
+
+            <div className="border-2 border-orange-500 rounded-lg bg-black grid grid-cols-1">
+                <img className="rounded-t-lg" src={project2} alt="" />
+                <p className="text-white text-2xl mt-6 mx-4">Tenzies Game made with React. Your Objective is to roll the dice till all the dice have same value.</p>
+                <p className="flex items-center text-xl text-orange-500 ml-4 mt-8">Made with : <img className="w-12 ml-4" src={react} alt="" /><img className="w-12" src={css} alt="" /></p>
+                <div className="flex items-center ml-4 mt-10 mb-6">
+                <a className="bg-orange-500 text-black px-16 py-4 rounded-lg w-1/5 flex items-center justify-center text-2xl mr-6 relative z-10 before:absolute before:inset-0 before:bg-black before:-z-10 before:border-2 before:border-orange-500 before:border-solid before:rounded-lg before:origin-left before:scale-x-0 before:transition-transform before:hover:scale-x-100 hover:text-orange-500" href="" target="_blank" rel="noopener noreferrer">Live</a>
+
+                <a className="bg-orange-500 text-black px-16 py-4 rounded-lg w-1/5 flex items-center justify-center text-2xl relative z-10 before:absolute before:inset-0 before:bg-black before:-z-10 before:border-2 before:border-orange-500 before:border-solid before:rounded-lg before:origin-left before:scale-x-0 before:transition-transform before:hover:scale-x-100 hover:text-orange-500" href="" target="_blank" rel="noopener noreferrer">Github</a>
+                </div>
+            </div>
+
+            <div className="border-2 border-orange-500 rounded-lg bg-black grid grid-cols-1">
+                <img className="rounded-t-lg" src={project3} alt="" />
+                <p className="text-white text-2xl mt-6 mx-4">A fun application to test how fast you can type in a given amount of time. It consists of a theme switcher too.</p>
+                <p className="flex items-center text-xl text-orange-500 ml-4 mt-8">Made with : <img className="w-12 ml-4" src={react} alt="" /><img className="w-12" src={css} alt="" /></p>
+                <div className="flex items-center ml-4 mt-10 mb-6">
+                <a className="bg-orange-500 text-black px-16 py-4 rounded-lg w-1/5 flex items-center justify-center text-2xl mr-6 relative z-10 before:absolute before:inset-0 before:bg-black before:-z-10 before:border-2 before:border-orange-500 before:border-solid before:rounded-lg before:origin-left before:scale-x-0 before:transition-transform before:hover:scale-x-100 hover:text-orange-500" href="" target="_blank" rel="noopener noreferrer">Live</a>
+
+                <a className="bg-orange-500 text-black px-16 py-4 rounded-lg w-1/5 flex items-center justify-center text-2xl relative z-10 before:absolute before:inset-0 before:bg-black before:-z-10 before:border-2 before:border-orange-500 before:border-solid before:rounded-lg before:origin-left before:scale-x-0 before:transition-transform before:hover:scale-x-100 hover:text-orange-500" href="" target="_blank" rel="noopener noreferrer">Github</a>
+                </div>
+            </div>
+
+            <div className="border-2 border-orange-500 rounded-lg bg-black grid grid-cols-1">
+                <img className="rounded-t-lg h-60 w-screen object-cover object-top" src={project4} alt="" />
+                <p className="text-white text-2xl mt-6 mx-4">Twimba is an Application that lets you add your own thoughts in the form of tweets and also lets you like, delete and even reply to a particular tweet.</p>
+                <p className="flex items-center text-xl text-orange-500 ml-4 mt-8">Made with : <img className="w-12 ml-4" src={html} alt="" /><img className="w-12" src={css} alt="" /><img className="w-12" src={javaScript} alt="" /></p>
+                <div className="flex items-center ml-4 mt-10 mb-6">
+                <a className="bg-orange-500 text-black px-16 py-4 rounded-lg w-1/5 flex items-center justify-center text-2xl mr-6 relative z-10 before:absolute before:inset-0 before:bg-black before:-z-10 before:border-2 before:border-orange-500 before:border-solid before:rounded-lg before:origin-left before:scale-x-0 before:transition-transform before:hover:scale-x-100 hover:text-orange-500" href="" target="_blank" rel="noopener noreferrer">Live</a>
+
+                <a className="bg-orange-500 text-black px-16 py-4 rounded-lg w-1/5 flex items-center justify-center text-2xl relative z-10 before:absolute before:inset-0 before:bg-black before:-z-10 before:border-2 before:border-orange-500 before:border-solid before:rounded-lg before:origin-left before:scale-x-0 before:transition-transform before:hover:scale-x-100 hover:text-orange-500" href="" target="_blank" rel="noopener noreferrer">Github</a>
+                </div>
+            </div>
+
+            <div className="border-2 border-orange-500 rounded-lg bg-black grid grid-cols-1">
+                <img className="rounded-t-lg h-60 object-cover" src={project5} alt="" />
+                <p className="text-white text-2xl mt-2 mx-4">A personal Dashboard which shows time, greeting message, weather and cryto value.</p>
+                <p className="flex items-center text-xl text-orange-500 ml-4 mt-8">Made with : <img className="w-12 ml-4" src={html} alt="" /><img className="w-12" src={css} alt="" /><img className="w-12" src={javaScript} alt="" /><img className="w-10" src={api} alt="" /></p>
+                <div className="flex items-center ml-4 mt-10 mb-6">
+                <a className="bg-orange-500 text-black px-16 py-4 rounded-lg w-1/5 flex items-center justify-center text-2xl mr-6 relative z-10 before:absolute before:inset-0 before:bg-black before:-z-10 before:border-2 before:border-orange-500 before:border-solid before:rounded-lg before:origin-left before:scale-x-0 before:transition-transform before:hover:scale-x-100 hover:text-orange-500" href="" target="_blank" rel="noopener noreferrer">Live</a>
+
+                <a className="bg-orange-500 text-black px-16 py-4 rounded-lg w-1/5 flex items-center justify-center text-2xl relative z-10 before:absolute before:inset-0 before:bg-black before:-z-10 before:border-2 before:border-orange-500 before:border-solid before:rounded-lg before:origin-left before:scale-x-0 before:transition-transform before:hover:scale-x-100 hover:text-orange-500" href="" target="_blank" rel="noopener noreferrer">Github</a>
+                </div>
+            </div>
+        </section>
+        <div className=" text-center transition-all delay-200 my-24 ease-in-out hover:scale-150 w-9/12 m-auto">
+        <a className="text-4xl bg-orange-500 text-black px-16 py-4 rounded-lg w-8/12 m-auto flex items-center justify-center relative z-10 before:absolute before:inset-0 before:bg-black before:-z-10 before:border-2 before:border-orange-500 before:border-solid before:rounded-lg before:origin-left before:scale-x-0 before:transition-transform before:hover:scale-x-100 hover:text-white" href="" target="_blank" rel="noopener noreferrer">Check more of my work on my Github <BsArrowRight className="ml-4" /></a>
+        </div>
+
+        <footer className="text-white py-20 text-center border-t-2 border-orange-500">
+            <h2 className="text-6xl mb-8">Sparked your Interest?</h2>
+            <h3 className="text-4xl mb-4">Let's Connect</h3>
+            <p className="underline text-orange-500 text-3xl">ayush.97mahajan@gmail.com</p>
+        </footer>
         </>
     )
 }
