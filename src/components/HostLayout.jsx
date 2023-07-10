d053733c7568fbdf1efa0de70920d34cc1a91259
Ayush1M <worklife2442@gmail.com>
2023-07-10 23:25:39 +0530

    updated styling for hostlayout.

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -4,9 +4,9 @@ import Header from "./Header";
 
 export default function HostLayout(){
     return(
-        <>
+        <div className="min-h-screen bg-light-black overflow-hidden font-display">
         <Header />
         <Outlet />
-        </>
+        </div>
     )
 }
