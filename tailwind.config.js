de64ec59b6eb640121f01d5f2f5745e7f1ed4e7d
Ayush1M <worklife2442@gmail.com>
2023-07-13 23:34:22 +0530

    Added themes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens:{
+      's' : '340px',
+      'sm' : '570px',
+      'md' : '800px',
+      'mdp' : '880px',
+      'lg' : '1024px',
+      'xl' : '1100px'
+    },
+    minWidth:{
+      '0' : '0px',
+      '5' : '550px'
+    },
+    fontFamily:{
+      'display': ['Roboto Condensed', 'sans-serif'],
+    },
     extend: {
-      colors: {
-        'white' : '#fff',
+      colors:{
+        'jet' : '#101010',
+        'light-black' : '#1C1C1C'
       },
     },
   },
